2b9431f588ff56c53774405803385c2eca5bdc92
vics-e <victor.egunlae@sage.com>
2026-06-25 05:26:12 +0100

    add Vercel web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 
+  import { Analytics } from "@vercel/analytics/react";
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <>
+      <App />
+      <Analytics />
+    </>
+  );
   
